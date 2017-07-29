5faa7f22ce615a0b9a00e31b5078d1f8eb765b32
iftimie <iftimie.alexandru.florentin@gmail.com>
2017-07-29 18:30:17 +0300

    printing ih,iw coco.py line 130

--- libs/datasets/coco.py
+++ libs/datasets/coco.py
@@ -127,6 +127,7 @@ def read(tfrecords_filename):
   gt_boxes = tf.reshape(gt_boxes, [num_instances, 5])
   gt_masks = tf.decode_raw(features['label/gt_masks'], tf.uint8)
   gt_masks = tf.cast(gt_masks, tf.int32)
+  print (ih,iw)
   gt_masks = tf.reshape(gt_masks, [17, ih, iw])
   ####################################################################be careful here. before 17 at the line above there was num_instances
 
